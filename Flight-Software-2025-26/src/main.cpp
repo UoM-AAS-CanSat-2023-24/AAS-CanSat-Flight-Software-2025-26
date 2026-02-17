b04de7958bb382e3798b7c2f57087a340e0657ed
Khalid <alnuaimykhalid@gmail.com>
2026-02-17 16:55:55 +0000

    IT BUILDS!!!

--- Flight-Software-2025-26/src/main.cpp
+++ Flight-Software-2025-26/src/main.cpp
@@ -6,17 +6,17 @@
 // will use (7,4) hamming code + overall parity checking = 8 bit codeword. this is to ensure single error correction, double error detection (SECDED)
 
 enum class MissionState : uint8_t { // each of these states uses (7,4) hamming codes which were encoded by hand: THESE NUMBERS ARE NOT RANDOM PLEASE UNDER NO CIRCUMSTANCES CHANGE THEM
-  LAUNCH_PAD_DISARMED = 0b00000000, // 0
-  LAUNCH_PAD_ARMED = 0b11010010 , // 210
-  ASCENT =  0b01010101, // 85
-  APOGEE =  0b10000111, // 135
-  DESCENT_PRE_PAYLOAD_RELEASE = 0b10011001, // 97
-  DESCENT_PAYLOAD_RELEASE = 0b01001011, // 75
-  DESCENT_PRE_PROBE_RELEASE = 0b0
-  DESCENT_PROBE_RELEASE = 0b11001100, // 108
-  DESCENT_POST_PROBE_RELEASE = 0b00011110, // 30
-  GROUNDED = 0b11100001, // 225
-  FAULT = 0b00110011 // 144 (this state is for when the system detects an error that it cannot correct, or if it detects an invalid state (should never happen))
+  LAUNCH_PAD_DISARMED = 0b00000000, //
+  LAUNCH_PAD_ARMED = 0b11010010, // 
+  ASCENT =  0b01010101, // 
+  APOGEE =  0b10000111, // 
+  DESCENT_PRE_PAYLOAD_RELEASE = 0b10011001, // 
+  DESCENT_PAYLOAD_RELEASE = 0b01001011, // 
+  DESCENT_PRE_PROBE_RELEASE = 0b11001100,
+  DESCENT_PROBE_RELEASE = 0b00011110, //
+  DESCENT_POST_PROBE_RELEASE = 0b11100001, // 
+  GROUNDED = 0b00110011, // 
+  FAULT = 0b10110100 //  (this state is for when the system detects an error that it cannot correct, or if it detects an invalid state (should never happen))
 };
 
   uint8_t fsw_state = static_cast<uint8_t>(MissionState::LAUNCH_PAD_DISARMED); // inital state
